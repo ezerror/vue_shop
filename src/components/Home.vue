2c9a836f2ac8483aecc8b9b7516c586577508236
ezerror <435719422@qq.com>
2020-02-10 21:03:57 +0800

    完成登录模块

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    logout: function () {
+      // 清空token
+      window.sessionStorage.clear()
+      // 跳转到登录页
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less">
+</style>
